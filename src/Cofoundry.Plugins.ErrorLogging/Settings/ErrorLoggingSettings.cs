f07291e1a6ff3358df6d8463dae7403f749cbc5a
Joel Mitchell <joel.mitchell@uberdigital.com>
2017-02-13 15:43:00 +0000

    Added readme and updated package references

--- src/Cofoundry.Plugins.ErrorLogging/Settings/ErrorLoggingSettings.cs
+++ src/Cofoundry.Plugins.ErrorLogging/Settings/ErrorLoggingSettings.cs
@@ -7,8 +7,15 @@ using System.Threading.Tasks;
 
 namespace Cofoundry.Plugins.ErrorLogging
 {
+    /// <summary>
+    /// Configuration settings for the Cofoundry.Plugins.ErrorLogging
+    /// </summary>
     public class ErrorLoggingSettings : PluginConfigurationSettingsBase
     {
+        /// <summary>
+        /// If set, an email notification will be sent to this address
+        /// every time an error occurs.
+        /// </summary>
         public string LogToEmailAddress { get; set; }
     }
 }
