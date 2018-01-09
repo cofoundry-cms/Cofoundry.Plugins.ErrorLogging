7a8d19b4d6a47150326b4f6faf91225f3562cdf7
Joel Mitchell <me@joelmitchell.co.uk>
2018-01-09 12:36:44 +0000

    * Fixed middleware ordering * Added minified module scripts

--- src/Cofoundry.Plugins.ErrorLogging/Middleware/ErrorLoggingMiddlewareStartupTask.cs
+++ src/Cofoundry.Plugins.ErrorLogging/Middleware/ErrorLoggingMiddlewareStartupTask.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 
 namespace Cofoundry.Plugins.ErrorLogging
 {
-    public class ErrorLoggingMiddlewareStartupTask : IStartupConfigurationTask
+    public class ErrorLoggingMiddlewareStartupTask : IRunAfterStartupConfigurationTask
     {
         public int Ordering
         {
             get { return (int)StartupTaskOrdering.First; }
         }
+
+        public Type[] RunAfter => new Type[] { typeof(ErrorHandlingMiddlewareConfigurationTask) };
 
         public void Configure(IApplicationBuilder app)
         {
